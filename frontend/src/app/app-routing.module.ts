2f2bb8d40e0f586fccecd0a9df62b69135bd0f37
Yone <chibariyo@gmail.com>
2022-01-22 18:26:16 -0300

    componente criar produto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { HomeComponent } from './components/views/home/home.component';
 import { ProductCrudComponent } from './components/views/product-crud/product-crud.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "products", component: ProductCrudComponent}
+  {path: "products", component: ProductCrudComponent},
+  {path: "products/create", component: ProductCreateComponent}
 ];
 
 @NgModule({
