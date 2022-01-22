9cef3287f86321a9b05efe52b4bfbc113bdc1eac
Yone <chibariyo@gmail.com>
2022-01-22 00:57:25 -0300

    navegação entre componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/views/home/home.component';
+import { ProductCrudComponent } from './components/views/product-crud/product-crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "products", component: ProductCrudComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
